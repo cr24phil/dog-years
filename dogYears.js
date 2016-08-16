77bac9b08f14e2994e7059ce01102ce9df7c7d31
Phil Pickering <cr24phil@gmail.com>
2016-08-16 21:58:19 +0100

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,8 @@
+// Dog Years Project
+
+var myAge = prompt("How old are you in human years?");
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+
+console.log("I am " + myAgeInDogYears + " years old in dog years.");
